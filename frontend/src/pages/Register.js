3bc0835b99df17868d95ba5197e944696c0dc933
ammananjum <emaananjum634@gmail.com>
2025-09-03 21:59:59 +0500

    Update Register.js

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -41,14 +41,14 @@ const Register = () => {
     setSuccess("");
 
     try {
-      await axios.post("http://localhost:5000/api/auth/register", {
+      await axios.post("https://9ace0c41-d172-46f8-ad9f-22a593437d12-00-2jpuy195o8qc9.sisko.replit.dev/api/auth/register", {
         name,
         email,
         password,
         role,
       });
 
-      // ✅ Do NOT auto-login. Show success message.
+      
       setSuccess("Registration successful! Please login now.");
       setName("");
       setEmail("");
